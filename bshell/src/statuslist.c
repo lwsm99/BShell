fe65bd22ba63812915201dc75fd07eee6029074c
Lucas <lucasweissmann@gmx.de>
2022-06-15 14:22:36 +0200

    Forgot to save

--- bshell/src/statuslist.c
+++ bshell/src/statuslist.c
@@ -3,7 +3,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "statuslist.h"
+#include "debug.h"
 
-void getStatusList(void) {
-    
+
+StatusList * list_append(void * element, StatusList * tail){
+    /*
+     * exchange only the list itself
+     *
+     */
+    StatusList * lst = malloc(sizeof(StatusList));
+    lst->head=element;
+    lst->tail=tail;
+    return lst;
+}
+
+StatusList * list_delete(void * element, StatusList * tail){
+    lst->head=element;
+    lst->tail=tail;
+    return lst;
 }
--- bshell/src/statuslist.h
+++ bshell/src/statuslist.h
@@ -2,5 +2,12 @@
 
 #define STATUSLIST_H
 
+typedef struct statuslist {
+    void *head;
+    struct statusList *tail;
+} StatusList;
+
+StatusList * list_append(void * head, StatusList * tail);
+StatusList * list_delete(void * head, StatusList * tail);
 
 #endif /* end of include guard: STATUSLIST_H */
