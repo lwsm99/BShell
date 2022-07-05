d9291afc805b3ace61efe0c8ad788f1a8561e0b6
Lz-01 <lennart.zinn@web.de>
2022-07-05 11:16:26 +0200

    added hu2

---
+++ hu2/memory/src/vmm.c
@@ -0,0 +1,37 @@
+#include "vmm.h"
+#include "output_utility.h"
+
+#include <stdio.h>
+char * pagetable;
+/**
+ * Initialized a statistics object to zero.
+ * @param stats A pointer to an uninitialized statistics object.
+ */
+static void statistics_initialize(Statistics *stats) {
+    stats->tlb_hits = 0;
+    stats->pagetable_hits = 0;
+    stats->total_memory_accesses = 0;
+}
+
+Statistics simulate_virtual_memory_accesses(FILE *fd_addresses, FILE *fd_backing) {
+    // Initialize statistics
+    Statistics stats;
+    statistics_initialize(&stats);
+
+    // TODO: Implement your solution
+create_pagetable();
+    return stats;
+}
+
+//translates the logical into physical address
+int translate_address( int address){
+int page_number = (address & 0x000FF00) >> 8;
+int offset = (address & 0x000000FF);
+printf("%d,%d\n", page_number, offset);
+}
+
+void create_pagetable(){
+    // size of pageTable is 256 Bytes(2^8)
+    int size = 256;
+    pagetable = malloc(256);
+}
